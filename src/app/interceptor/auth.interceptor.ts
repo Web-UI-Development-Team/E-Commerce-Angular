a3fa08f1d8c16184288592d553cc6d26ba00780a
MariamElmsery <mariamelmsery7@gmail.com>
2024-03-21 15:20:14 +0200

    jwt

--- src/app/interceptor/auth.interceptor.ts
+++ src/app/interceptor/auth.interceptor.ts
@@ -17,7 +17,7 @@ export class AuthInterceptor implements HttpInterceptor {
   ): Observable<HttpEvent<any>> {
     req = req.clone({
       setHeaders: {
-        jwt: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IndhbGVlZC5hbG1lbmF3eUBvdXRsb29rLmNvbSIsImlhdCI6MTcxMDc3NTYwMCwiZXhwIjoxNzEwNzc5MjAwfQ.V0cU-W-5Ey4apiJs02207x_7rDDrUkfwY6hCRpMQGe0',
+        jwt: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IndhbGVlZC5hbG1lbmF3eUBvdXRsb29rLmNvbSIsImlhdCI6MTcxMTAyNjA3NiwiZXhwIjoxNzExMDI5Njc2fQ.VkAUlI09abwX_TsJ72C-vArMJbB_B7ROJ1dHCf9YvzQ',
       },
     });
 
